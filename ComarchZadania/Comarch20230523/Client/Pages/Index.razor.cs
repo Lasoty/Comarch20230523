c45c0e464147f42dcb17506c11aa0d0ac80eb07f
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-05-23 14:24:21 +0200

    Binding jednostronny i zdarzenia.

---
+++ ComarchZadania/Comarch20230523/Client/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Comarch20230523.Client.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        int LastMultimpleOfThree = 0;
+
+        private void UpdateLastOfMultipleOfThree(int value)
+        {
+            LastMultimpleOfThree = value;
+        }
+    }
+}
