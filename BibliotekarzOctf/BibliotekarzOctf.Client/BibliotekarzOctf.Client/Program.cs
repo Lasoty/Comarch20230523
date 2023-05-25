193a33a2cc929689e7251ab17f271b5b04f3cd89
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-05-25 12:13:44 +0200

    Uwierzytelnianie klienta

--- BibliotekarzOctf/BibliotekarzOctf.Client/BibliotekarzOctf.Client/Program.cs
+++ BibliotekarzOctf/BibliotekarzOctf.Client/BibliotekarzOctf.Client/Program.cs
@@ -3,13 +3,26 @@ using Blazorise;
 using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("BibliotekarzOctf.Server", client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BibliotekarzOctf.Server"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://82e68d04-499e-4c67-adb3-5cc041a9b425/API.Access");
+});
+
+var temp = builder.Configuration.GetSection("AzureAd").GetValue<string>("ClientId");
 
 AddBlazorise(builder.Services);
 
